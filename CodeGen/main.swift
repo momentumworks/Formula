1a17cf63e1a237d2bd80fd90a10cc6ba8346f91a
rheese.burgess <rheese.burgess@googlemail.com>
2016-03-23 17:38:17 +0000

    Changing the codegen project to basically be a windowless app

---
+++ CodeGen/main.swift
@@ -0,0 +1,59 @@
+//
+//  main.swift
+//  codegen-cli
+//
+//  Created by Krzysztof Zabłocki on 23/03/16.
+//  Copyright © 2016 Momentumworks. All rights reserved.
+//
+
+import Foundation
+import SourceKittenFramework
+
+let GeneratedCodeDirectory = "Autogen"
+let GeneratedCodeFile = "Autogen.swift"
+
+private func generateForDirectory(directory: String, usingGenerators generators: [Generator], cleanFirst: Bool) {
+  let trimmedTarget = Utils.removeTrailingFileSeparator(directory)
+  let outputDirectory = "\(trimmedTarget)/\(GeneratedCodeDirectory)"
+  let outputFile = "\(outputDirectory)/\(GeneratedCodeFile)"
+  
+  if (cleanFirst) { Utils.deleteFile(outputFile) }
+  
+  NSLog("About to generate extensions for directory \(directory) using generators \(generators.map{ String($0.dynamicType) })")
+  
+  let generator = CodeGenerator(generators: generators)
+  let generated = generator.generateForDirectory(trimmedTarget)
+  
+  Utils.createDirectoryIfNonExistent(outputDirectory)
+  NSLog("Writing file \(outputFile)")
+  try! generated.writeToFile(outputFile, atomically: true, encoding: NSUTF8StringEncoding)
+  
+  NSLog("Finished generating extensions")
+}
+
+func main() {
+  var targetArgument: String?
+  var cleanFirst = false
+
+  for (idx, argument) in Process.arguments.enumerate() {
+    switch argument {
+    case "-target":
+      targetArgument = Process.arguments[idx + 1]
+    case "-clean":
+      cleanFirst = true
+    default:
+      break
+    }
+  }
+
+  guard let target = targetArgument else {
+    print("Usage: \"codegen -path <path-directory> [-clean]\"")
+    return
+  }
+
+  NSLog("Running code gen with target \(target) \(cleanFirst ? "(cleaning first)" : "")")
+  let generators = [ImmutableSettersGenerator()]
+  generateForDirectory(target, usingGenerators: generators, cleanFirst: cleanFirst)
+}
+
+main()
