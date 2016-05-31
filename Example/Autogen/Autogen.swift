71f58bf09639184e4aa522e0e665ff834cfc1bf6
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-05-31 18:46:05 +0100

    fix(AutoEquatable): modified stencil so now the output is correct

--- Example/Autogen/Autogen.swift
+++ Example/Autogen/Autogen.swift
@@ -2,46 +2,63 @@
 import AppKit
 // MARK: - AutoEquatable
 
+
+extension EnumCase: Equatable {}
+public func ==(lhs: EnumCase, rhs: EnumCase) -> Bool {
+
+  
+
+  return lhs.name == rhs.name &&
+  lhs.associatedValues == rhs.associatedValues &&
+   true
+  
+}
+
+extension Field: Equatable {}
+public func ==(lhs: Field, rhs: Field) -> Bool {
+
+  
+
+  return lhs.accessibility == rhs.accessibility &&
+  lhs.name == rhs.name &&
+  lhs.type == rhs.type &&
+   true
+  
+}
+
 extension FifthThing: Equatable {}
  func ==(lhs: FifthThing, rhs: FifthThing) -> Bool {
-  
-  
+
   
   switch (lhs, rhs) {
   
   case let (.One(lhsValue1), .One(rhsValue1)) where lhsValue1 == rhsValue1 && true :
     return true
   
-     default: return false
-  }
-  
-  
-  
-  switch (lhs, rhs) {
-  
   case let (.Two(lhsValue1), .Two(rhsValue1)) where lhsValue1 == rhsValue1 && true :
     return true
-  
-     default: return false
-  }
-  
-  
-  
-  switch (lhs, rhs) {
   
   case let (.Three, .Three):
     return true
   
-     default: return false
+  default: return false
   }
   
-  
-
-  
-
 }
 
-// MARK: -
+extension Type: Equatable {}
+public func ==(lhs: Type, rhs: Type) -> Bool {
+
+  
+
+  return lhs.accessibility == rhs.accessibility &&
+  lhs.name == rhs.name &&
+  lhs.extensions == rhs.extensions &&
+  lhs.kind == rhs.kind &&
+   true
+  
+}
+
 
 // MARK: - Immutable
 
