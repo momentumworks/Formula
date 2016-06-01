f93971a0c4eb73d2111eb107c856e02bc9fa9123
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-06-01 11:40:41 +0100

    tests(Enum): added more SimpleEnum & ComplexEnum test cases

--- CodeGenTests/ExtractorTests.swift
+++ CodeGenTests/ExtractorTests.swift
@@ -33,10 +33,50 @@ class ExtractorTests: QuickSpec {
           EnumCase(name: "Two", associatedValues: []),
           EnumCase(name: "Three", associatedValues: [])
         ]
-        let kind = Kind.Enum(enumCases)
-//        expect(metaData["Anenum"]?.kind).to(equal(kind))
+        expect(metaData["Anenum"]?.kind).to(equal(Kind.Enum(enumCases)))
       }
+      
+      it("should recognize its extensions") {
+        let extensions: Set<Extension> = [
+          "Protocol1",
+          "Protocol2"
+        ]
+        expect(metaData["Anenum"]?.extensions).to(equal(extensions))
+      }
+
     }
+    
+    describe("when extracting complex enums") {
+      
+      let file = File(path: self.testBundle.pathForResource("ComplexEnum", ofType: "fixture")!)!
+      let metaData = Extractor.extractTypes([file])
+      
+      it("should recognize its name & type") {
+        expect(metaData["AComplexEnum"]).toNot(beNil())
+        expect(metaData["AComplexEnum"]?.kind.isEnum).to(equal(true))
+      }
+      
+      it("should recognize its cases") {
+        let enumCases = [
+          EnumCase(name: "One", associatedValues: ["Int"]),
+          EnumCase(name: "Two", associatedValues: ["Int", "Int"]),
+          EnumCase(name: "Three", associatedValues: ["String", "Int", "String"]),
+          EnumCase(name: "Four", associatedValues: [])
+        ]
+        expect(metaData["AComplexEnum"]?.kind).to(equal(Kind.Enum(enumCases)))
+      }
+      
+      it("should recognize its extensions") {
+        let extensions: Set<Extension> = [
+          "Protocol1",
+          "Protocol2",
+          "Protocol3"
+        ]
+        expect(metaData["AComplexEnum"]?.extensions).to(equal(extensions))
+      }
+      
+    }
+
   
   }
   
