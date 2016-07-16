17aa8252f1c5b7793c50ae13aa7704f30f349c75
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-07-16 14:46:34 +0100

    feat(Templates): ImmutableTemplate.js now fully functional

--- CodeGenTests/ImmutableTemplate.js
+++ CodeGenTests/ImmutableTemplate.js
@@ -3,7 +3,17 @@
 return extensions.Immutable.map(function(object) {
   return `extension ${object.name} {\n`
     + object.fields.map(function(field) {
-      return `${field.accessibility} func set(${field.name} ${field.name}: ${field.type }) -> ${object.name} { return self; }\n`
+      return `${field.accessibility} func set(${field.name} ${field.name}: ${field.type }) -> ${object.name} { return `
+        + constructorCall(object)
+        + '}\n'
     })
     + '}\n\n';
 })
+
+
+function constructorCall(object) {
+    return object.name
+    + '('
+    + object.fields.map(function(field) { return `${field.name}: ${field.name}` }).join(',')
+    + ')'
+}
