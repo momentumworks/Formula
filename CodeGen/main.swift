e4509c13fe82de65858717b0a38ac118017095d8
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-08-19 11:32:14 +0100

    feat(Main): added support for multiple passes

--- CodeGen/main.swift
+++ CodeGen/main.swift
@@ -7,14 +7,42 @@ let GeneratedCodeDirectory = "Autogen"
 let GeneratedCodeFile = "Autogen.swift"
 
 
-private func generate(extractorEngines extractorEngines: [ExtractorEngine], templateEngines: [TemplateEngine], sourceDirectory: String, usingTemplates templates: [Path], cleanFirst: Bool) {
+private func generateFirstPass(fromDirectory sourceDirectory: String, extractorEngines: [ExtractorEngine], templateEngines: [TemplateEngine], usingTemplates templates: [Path], cleanFirst: Bool) -> Path {
   print("About to generate extensions for directory \(sourceDirectory) using templates \(templates)")
   
   let files = FileUtils.fullPathForAllFilesAt(sourceDirectory, withExtension: "swift", ignoreSubdirectory: GeneratedCodeDirectory)
-  let filesByExtension  = files
+  let generated = generateCode(fromSource: files, extractorEngines: extractorEngines, templateEngines: templateEngines, usingTemplates: templates)
+  
+  let trimmedTarget = FileUtils.removeTrailingFileSeparator(sourceDirectory)
+  let outputDirectory = "\(trimmedTarget)/\(GeneratedCodeDirectory)"
+  let outputFile = "\(outputDirectory)/\(GeneratedCodeFile)"
+  if (cleanFirst) {
+    FileUtils.deleteFile(outputFile)
+  }
+  print("Writing file \(outputDirectory)/\(GeneratedCodeFile)")
+  FileUtils.mkdirAndWriteFile(fileName: GeneratedCodeFile, inDirectory: outputDirectory, content: generated)
+
+  print("Finished generating extensions")
+  return Path(outputFile)
+}
+
+
+private func generateSecondaryPass(fromSource source: Path, extractorEngines: [ExtractorEngine], templateEngines: [TemplateEngine], usingTemplates templates: [Path]) {
+  
+  let originalContent: String = try! source.read()
+  let generated = generateCode(fromSource: [source], extractorEngines: extractorEngines, templateEngines: templateEngines, usingTemplates: templates)
+
+  try! source.delete()
+  try! source.write(originalContent + generated)
+}
+
+
+
+private func generateCode(fromSource source: [Path], extractorEngines: [ExtractorEngine], templateEngines: [TemplateEngine], usingTemplates templates: [Path]) -> String {
+  let filesByExtension  = source
     .filter { $0.fileExtension != nil }
     .arrayGroupBy { [$0.fileExtension!] }
-
+  
   let types = filesByExtension.reduce([Type]()) { working, entry in
     let (ext, paths) = entry
     let engine = extractorEngines.find({ $0.fileExtension == ext })
@@ -23,7 +51,7 @@ private func generate(extractorEngines extractorEngines: [ExtractorEngine], temp
     }
     return working
   }
-
+  
   let imports = filesByExtension.reduce([Import]()) { working, entry in
     let (ext, paths) = entry
     let engine = extractorEngines.find({ $0.fileExtension == ext })
@@ -37,7 +65,7 @@ private func generate(extractorEngines extractorEngines: [ExtractorEngine], temp
     .filter { $0.fileExtension != nil }
     .arrayGroupBy { [$0.fileExtension!] }
   
-  let generated = templatesByExtension.reduce("") { working, entry in
+  return templatesByExtension.reduce("") { working, entry in
     let (ext, paths) = entry
     if let engine = templateEngines.find({ $0.templateExtension == ext }) {
       return working + engine.generateForFiles(types, imports: imports, templates: paths)
@@ -45,22 +73,14 @@ private func generate(extractorEngines extractorEngines: [ExtractorEngine], temp
     return working
   }
 
-  let trimmedTarget = FileUtils.removeTrailingFileSeparator(sourceDirectory)
-  let outputDirectory = "\(trimmedTarget)/\(GeneratedCodeDirectory)"
-  let outputFile = "\(outputDirectory)/\(GeneratedCodeFile)"
-  if (cleanFirst) {
-    FileUtils.deleteFile(outputFile)
-  }
-  print("Writing file \(outputDirectory)/\(GeneratedCodeFile)")
-  FileUtils.mkdirAndWriteFile(fileName: GeneratedCodeFile, inDirectory: outputDirectory, content: generated)
-
-  print("Finished generating extensions")
 }
+
 
 struct Configuration {
   let sourceDirectory: String
   let templates: String
   let cleanFirst: Bool
+  let passes: Int
 }
 
 
@@ -68,6 +88,7 @@ private func parseArgs() -> Configuration? {
   var targetArgument: String?
   var templatesArgument: String?
   var cleanFirst = false
+  var passes = 1
   
   for (idx, argument) in Process.arguments.enumerate() {
     switch argument {
@@ -77,6 +98,8 @@ private func parseArgs() -> Configuration? {
       templatesArgument = Process.arguments[idx + 1]
     case "-clean":
       cleanFirst = true
+    case "-passes":
+      passes = Int(Process.arguments[idx + 1])!
     default:
       break
     }
@@ -86,7 +109,8 @@ private func parseArgs() -> Configuration? {
     Configuration(
       sourceDirectory: $0,
       templates: templatesArgument ?? FileUtils.pathFromWorkingDirectory("/Templates"),
-      cleanFirst: cleanFirst
+      cleanFirst: cleanFirst,
+      passes: passes
     )
   }
 }
@@ -98,7 +122,7 @@ func main() {
   let templateEngines : [TemplateEngine] = [JavascriptEngine(), StencilEngine()]
 
   guard let config = parseArgs() else {
-    print("Usage: \"codegen -target <path-directory> [-clean] [-templates <template-directory>]\"")
+    print("Usage: \"codegen -target <path-directory> [-clean] [-templates <template-directory>] [-passes <number-of-passes>]\"")
     return
   }
 
@@ -110,7 +134,14 @@ func main() {
     withExtension: nil,
     ignoreSubdirectory: GeneratedCodeDirectory
   )
-  generate(extractorEngines: extractorEngines, templateEngines: templateEngines, sourceDirectory: config.sourceDirectory, usingTemplates: templates, cleanFirst: config.cleanFirst)
+  let generatedFilePath = generateFirstPass(fromDirectory: config.sourceDirectory, extractorEngines: extractorEngines, templateEngines: templateEngines, usingTemplates: templates, cleanFirst: config.cleanFirst)
+  
+  if config.passes > 1 {
+    for _ in 1...config.passes {
+      generateSecondaryPass(fromSource: generatedFilePath, extractorEngines: extractorEngines, templateEngines: templateEngines, usingTemplates: templates)
+    }
+  }
+  
 }
 
 let inTests = NSClassFromString("XCTest") != nil
